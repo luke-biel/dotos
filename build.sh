6d2be9dd8dbabbcccca8ee2e1e8cfb2e71efbf1f
Łukasz Biel <lukasz.p.biel@gmail.com>
2021-09-14 00:06:41 +0200

    Compile as PiC

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-export RUSTFLAGS="-C target-cpu=cortex-a53 -C link-arg=-Tsrc/bsp/rpi3/link.ld -C link-arg=-otarget/kernel.elf"
+export RUSTFLAGS="-C target-cpu=cortex-a53 -C link-arg=-Tsrc/bsp/rpi3/link.ld -C link-arg=-otarget/kernel.elf -C relocation-model=pic"
 
 cargo rustc --target=aarch64-unknown-none-softfloat --release --features=rpi3 --no-default-features
 
